0200c19821b53a5752f8209f9c9410af43e33f35
Greg Stephens <greg@udon.org>
2018-07-10 19:58:16 -0700

    0.2.3

--- graphql.js
+++ graphql.js
@@ -3,7 +3,7 @@ module.exports = function(RED) {
   var axios = require("axios");
   var mustache = require("mustache");
 
-  var vers = "0.0.4";
+  var vers = "0.2.3";
 
   function safeJSONStringify(input, maxDepth) {
     var output,
