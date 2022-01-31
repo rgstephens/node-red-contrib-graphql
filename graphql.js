e2c63c03d7e98f74ee19bcf71641e506b229922b
Greg Stephens <g.stephens@rasa.com>
2022-01-30 22:45:44 -0800

    remove ghin examples

--- graphql.js
+++ graphql.js
@@ -3,7 +3,7 @@ module.exports = function(RED) {
   var axios = require("axios");
   var mustache = require("mustache");
 
-  var vers = "1.4.0";
+  var vers = "1.4.1";
 
   function isReadable(value) {
     return typeof value === 'object' && typeof value._read === 'function' && typeof value._readableState === 'object'
